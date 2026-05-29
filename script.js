4587d9fa3b18d9724e221fbc6c8ad9d7bd7293f1
Nascimento-Hebo <nascimentohebo2@gmail.com>
2026-05-29 17:49:56 -0300

    feat: adicionei nova funcionalidade

---
+++ script.js
@@ -0,0 +1,11 @@
+function selectNumber() {
+    const min = Math.ceil(document.getElementById("inputMin").value);
+    const max = Math.floor(document.getElementById("inputMax").value);
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+
+    const drawnNumber = document.getElementById("ResultNumber");
+    drawnNumber.innerHTML = result
+
+}
