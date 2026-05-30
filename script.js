c5f362f9561b610aee1cbbfbac057c4429f7d6bf
Nascimento-Hebo <nascimentohebo2@gmail.com>
2026-05-30 12:25:48 -0300

    feat:adicionei nova funcionalidade

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ function selectNumber() {
     const min = Math.ceil(document.getElementById("inputMin").value);
     const max = Math.floor(document.getElementById("inputMax").value);
 
-    if (min >= max) {
-        alert("O valor mínimo, não pode ser maior ou igual que o valor máximo")
+    if (min > max) {
+        alert("O campo mínimo, não pode ser maior que o campo máximo")
+    } else if (min == max) {
+        alert("Os dois campos não podem ter números iguais")
+    } else if (min == "" || max == "") {
+        alert("Não pode ter campo vazio");
     } else {
 
         const result = Math.floor(Math.random() * (max - min + 1)) + min;
