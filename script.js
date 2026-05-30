028f312a14ef53cc7164c9bb239505825a955b3d
Nascimento-Hebo <nascimentohebo2@gmail.com>
2026-05-30 11:02:46 -0300

    feat: adicionei uma nova funcionalidade

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ function selectNumber() {
     const min = Math.ceil(document.getElementById("inputMin").value);
     const max = Math.floor(document.getElementById("inputMax").value);
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert("O valor mínimo, não pode ser maior ou igual que o valor máximo")
+    } else {
+
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        const drawnNumber = document.getElementById("ResultNumber");
+        drawnNumber.innerHTML = result;
+        drawnNumber.style.color = white;
+    }
 
 
-    const drawnNumber = document.getElementById("ResultNumber");
-    drawnNumber.innerHTML = result
-    drawnNumber.style.color = white
 
 
 }
